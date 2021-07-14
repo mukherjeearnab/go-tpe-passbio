3899ac5c8d16f87ba962e85cd291a2e014dc8f9c
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-07-14 14:55:19 +0530

    Added Import Key Function, Few Comments

--- key_gen.go
+++ key_gen.go
@@ -7,10 +7,12 @@ import (
 	"math/rand"
 )
 
+// Parent Struct
 type SecretKey struct {
 	key Key
 }
 
+// Key Struct
 type Key struct {
 	M_1  mat.Matrix `json:"m1"`
 	M_2  mat.Matrix `json:"m2"`
@@ -19,6 +21,7 @@ type Key struct {
 	Pi   []int      `json:"pi"`
 }
 
+// Export Key Format Struct
 type exportKey struct {
 	M_1  []byte `json:"m1"`
 	M_2  []byte `json:"m2"`
@@ -29,14 +32,20 @@ type exportKey struct {
 
 // Generate New Secret Key
 func (sk *SecretKey) KeyGen(seed int64, matrixSize int) {
+	// Set Random Seed
 	rand.Seed(seed)
+
+	// Generate Random Non-singular Matrices
 	sk.key.M_1, sk.key.M_1i = generateMatrix(matrixSize)
 	sk.key.M_2, sk.key.M_2i = generateMatrix(matrixSize)
+
+	// Generate Permutation 'pi'
 	sk.key.Pi = generatePermutation(matrixSize)
 }
 
 // Export Secret Key as JSON string
 func (sk *SecretKey) ExportKey() string {
+	// Marshal mat.Matrix to byte[]
 	M_1Bytes, err := mat.DenseCopyOf(sk.key.M_1).MarshalBinary()
 	if err != nil {
 		fmt.Printf("BinMarshall Error: %v", err)
@@ -58,9 +67,34 @@ func (sk *SecretKey) ExportKey() string {
 		return ""
 	}
 
+	// Marshal byte[] struct to JSON string
 	expKey := exportKey{M_1Bytes, M_1iBytes, M_2Bytes, M_2iBytes, sk.key.Pi}
 	keyJSON, _ := json.Marshal(expKey)
 	return string(keyJSON)
+}
+
+// Import Secret Key from JSON string
+func (sk *SecretKey) ImportKey(JSON string) {
+	// JSON Unmarshal
+	var importKey exportKey
+	json.Unmarshal([]byte(JSON), &importKey)
+
+	// Unmarshal byte[] to mat.Dense
+	var M_1 mat.Dense
+	M_1.UnmarshalBinary(importKey.M_1)
+	var M_1i mat.Dense
+	M_1i.UnmarshalBinary(importKey.M_1i)
+	var M_2 mat.Dense
+	M_2.UnmarshalBinary(importKey.M_2)
+	var M_2i mat.Dense
+	M_2i.UnmarshalBinary(importKey.M_2i)
+
+	// Set sk Vars
+	sk.key.M_1 = &M_1
+	sk.key.M_1i = &M_1i
+	sk.key.M_2 = &M_2
+	sk.key.M_2i = &M_2i
+	sk.key.Pi = importKey.Pi
 }
 
 /* ***************************
@@ -70,13 +104,20 @@ func (sk *SecretKey) ExportKey() string {
 // Generate A Random Non-singular Matrix
 func generateMatrix(matrixSize int) (a, aInv mat.Matrix) {
 	for {
+		// Generate Random slice
 		data := make([]float64, matrixSize*matrixSize)
 		for i := range data {
 			data[i] = float64(rand.Int())
 		}
+
+		// Generate Random Matrix
 		A := mat.NewDense(matrixSize, matrixSize, data)
+
+		// Check of Inverse Exists
 		var A_Inv mat.Dense
 		err := A_Inv.Inverse(A)
+
+		// If Inverse Exists, return the Matrix
 		if err == nil {
 			return A, &A_Inv
 		}
@@ -86,10 +127,13 @@ func generateMatrix(matrixSize int) (a, aInv mat.Matrix) {
 
 // Generate a Random Permutation
 func generatePermutation(matrixSize int) []int {
+	// Generate Ordered Slice
 	Permutation := make([]int, matrixSize)
 	for i := 0; i < matrixSize; i++ {
 		Permutation[i] = i
 	}
+
+	// Randomize the Order to generate Permutation
 	rand.Shuffle(matrixSize, func(i, j int) { Permutation[i], Permutation[j] = Permutation[j], Permutation[i] })
 	return Permutation
 }
