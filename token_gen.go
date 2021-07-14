c2032264f467c4d9c1433f81f54636686d58afaa
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-07-14 19:56:46 +0530

    Added TokenGen Function

---
+++ token_gen.go
@@ -0,0 +1,71 @@
+package tpe
+
+import (
+	"encoding/base64"
+	"fmt"
+	"gonum.org/v1/gonum/mat"
+	"math/rand"
+	"time"
+)
+
+// TokenGen vector --> y
+func (tpe *TPE) TokenGen(Y []float64) string {
+	// Initial Check (Check Vector Length)
+	if len(Y) != tpe.setup.N {
+		fmt.Printf("ERROR! Vector (%d) length not equal to Set (%d) Length", len(Y), tpe.setup.N)
+	}
+
+	// Set Random Seed
+	rand.Seed(time.Now().UnixNano())
+
+	// Step 1: Generate Random Numbers alpha, ry
+	alpha := generateNumber(true)
+	ry := generateNumber(false)
+
+	// Step 2: Generate new vector y' with len = n+3
+	Y_dash := make([]float64, len(Y)+3)
+	for i := 0; i < len(Y); i++ {
+		Y_dash[i] = Y[i] * alpha
+	}
+	Y_dash[len(Y)] = alpha
+	Y_dash[len(Y)+1] = 0
+	Y_dash[len(Y)] = ry
+
+	// Step 3: Permute y' to pi(y') = y''
+	Y_dd := make([]float64, len(Y_dash))
+	for i := 0; i < len(Y_dash); i++ {
+		Y_dd[tpe.key.Pi[i]] = Y_dash[i]
+	}
+
+	// Step 4: Transform y'' to a Diagonal Matrix with diag(Matrix) = y''
+	Y_Diag := mat.NewDiagDense(len(Y_dd), Y_dd)
+
+	// Step 5: Generate a Random Lower Triangular Matrix with diagonal elements = 1
+	_Sy := generateLTMatrix(tpe.setup.N + 3)
+	var Sy mat.Dense
+	Sy.CloneFrom(_Sy)
+	for i := 0; i < tpe.setup.N+3; i++ {
+		Sy.Set(i, i, 1)
+	}
+
+	// Step 6: Compute Token Ty = M_2i Y_Diag Sy M_1i
+	var Ty mat.Dense
+	Ty.Mul(tpe.key.M_2i, Y_Diag)
+	Ty.Mul(&Ty, &Sy)
+	Ty.Mul(&Ty, tpe.key.M_1i)
+
+	// Convert Ty to Byte[]
+	TyBytes, err := Ty.MarshalBinary()
+	if err != nil {
+		fmt.Printf("BinMarshall Error: %v", err)
+		return "ERROR!"
+	}
+
+	// Return Base64-String of TyBytes
+	return base64.StdEncoding.EncodeToString(TyBytes)
+}
+
+/* ***************************
+   Auxiliary Functions
+   *************************** */
+// NONE
