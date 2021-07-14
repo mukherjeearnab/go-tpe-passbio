0399e7d56d74bbe7d198e61dd4803b9a3cb33ee1
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-07-14 19:35:07 +0530

    Completed Encrypt Function

--- encrypt.go
+++ encrypt.go
@@ -1,6 +1,8 @@
 package tpe
 
 import (
+	"encoding/base64"
+	"fmt"
 	"gonum.org/v1/gonum/mat"
 	"math"
 	"math/rand"
@@ -36,9 +38,32 @@ func (tpe *TPE) Encrypt(X []float64) string {
 		X_dd[tpe.key.Pi[i]] = X_dash[i]
 	}
 
-	// TODO: Steps 4,5,6
+	// Step 4: Transform x'' to a Diagonal Matrix with diag(Matrix) = x''
+	X_Diag := mat.NewDiagDense(len(X_dd), X_dd)
 
-	return ""
+	// Step 5: Generate a Random Lower Triangular Matrix with diagonal elements = 1
+	_Sx := generateLTMatrix(tpe.setup.N + 3)
+	var Sx mat.Dense
+	Sx.CloneFrom(_Sx)
+	for i := 0; i < tpe.setup.N+3; i++ {
+		Sx.Set(i, i, 1)
+	}
+
+	// Step 6: Compute Cipher = M_1 Sx X_Diag M_2
+	var Cx mat.Dense
+	Cx.Mul(tpe.key.M_1, &Sx)
+	Cx.Mul(&Cx, X_Diag)
+	Cx.Mul(&Cx, tpe.key.M_2)
+
+	// Convert Cx to Byte[]
+	CxBytes, err := Cx.MarshalBinary()
+	if err != nil {
+		fmt.Printf("BinMarshall Error: %v", err)
+		return "ERROR!"
+	}
+
+	// Return Base64-String of CxBytes
+	return base64.StdEncoding.EncodeToString(CxBytes)
 }
 
 /* ***************************
@@ -52,5 +77,21 @@ func generateNumber(positive bool) float64 {
 		return math.Abs(randomNum)
 	} else {
 		return randomNum
+	}
+}
+
+// Generate A Random Lower Triangular Matrix
+func generateLTMatrix(matrixSize int) mat.Matrix {
+	for {
+		// Generate Random slice
+		data := make([]float64, matrixSize*matrixSize)
+		for i := range data {
+			data[i] = float64(rand.Int())
+		}
+
+		// Generate Random Matrix
+		A := mat.NewTriDense(matrixSize, false, data)
+
+		return A
 	}
 }
